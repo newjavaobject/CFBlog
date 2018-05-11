d5bc33b8a18547dc13dbb240d40d2096e932e789
chenzhiyu <nlxxttxs_czy@163.com>
2018-05-11 14:54:20 +0800

    spring源码解析 --> 自定义标签

--- src/main/test/com/cf/blog/UserParser.java
+++ src/main/test/com/cf/blog/UserParser.java
@@ -1,10 +1,7 @@
 package com.cf.blog;
 
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
-import org.springframework.beans.factory.xml.BeanDefinitionParser;
-import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 /**
